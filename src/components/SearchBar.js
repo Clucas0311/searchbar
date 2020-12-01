f6895bb93154404dba8542d9c9c9dbceb5279180
Clucas0311 <clucas0311@gmail.com>
2020-11-30 20:58:11 -0500

    Understanding event handling in react

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+	state = { term: '' };
+
 	render() {
 		return (
 			<div className="search bar ui segment">
 				<form className="ui form">
 					<div className="field">
-						<input type="text" placeholder="Search..." />
+						<label>Search Bar</label>
+						<input
+							type="text"
+							// every time the react rerenders it will update with the new value
+							value={this.state.term}
+							// As Soon as the user types into the search bar callback gets called
+							// Pull the change event the user did out of the object and store it as the new value
+							onChange={(event) => this.setState({ term: event.target.value })}
+							placeholder="Search..."
+						/>
 					</div>
 				</form>
 			</div>
